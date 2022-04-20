e6ecff20d41e04d11676dd1c504ac1c75228965a
ElissonRodrigues <elissonrodrigues98@gmail.com>
2022-04-20 10:35:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,18 @@
 #!/bin/bash
 
 cd
+nome=proot-distro
 
 apt update
-apt install proot-distro
-proot-distro install ubuntu
 
+pacote=$(dpkg --get-selections | grep $nome )
+
+if [ -n "$pacote" ] ;  
+then echo;
+     echo "$nome is already installed"; 
+else echo;
+     echo "$nome Is not installed"
+     apt install $nome
+
+proot-distro install ubuntu 
 proot-distro login ubuntu -- apt update && apt full-upgrade && apt install mame-tools
